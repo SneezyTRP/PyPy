0ec4739fe49858d1cd21d91cd35666926899eef0
SneezyTRP <rohangeorge1106@gmail.com>
2022-04-25 15:35:07 -0500

    added lists and diff data structures

--- DataTypes.py
+++ DataTypes.py
@@ -1,11 +1,23 @@
 # Review of the common python data types and data structures with applications
 
 # Write a list
-
+food = ["apple", "chicken", "banana"]
+names = ["Brandon", "Rohan", "Ben"]
+#List Operations
+#Addition
+foodnames = food + names
+print(food[:2]) 
+print(names[1:3])
+print(foodnames[:2])
 # Rohan
+countries=("South Africa", "USA", "England", "India",)
+animals = ("Bear", "Dog", "Lion", "Zebra")
+animalsc= countries + animals
+print(animalsc)
 
 
 # Rayhan
+car = {"key": "Value"}
 
 
 # Write a set
