2e5d372d8dca451d54f940464f723dd33f569d39
AlphonseBrandon <alphonse.brand@gmail.com>
2022-04-25 17:22:38 +0100

    To do for Rohan and Rayhan

---
+++ DataTypes.py
@@ -0,0 +1,36 @@
+# Review of the common python data types and data structures with applications
+
+# Write a list
+
+# Rohan
+
+
+# Rayhan
+
+
+# Write a set
+
+# Rohan
+
+
+# Rayhan
+
+
+# Write a dictionary
+
+# Rohan
+
+
+# Rayhan
+
+
+# Write a turple
+
+# ROhan
+
+
+# Rayhan
+
+
+
+
